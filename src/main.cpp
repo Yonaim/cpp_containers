3234aa8f9440263187511f9f20c0c472149804a5
Yonaim <yonaimyo@gmail.com>
2025-10-08 20:42:10 +0900

    Feat: call test functions in main

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+void test_lexicographical_compare();
+void test_pair();
+void test_equal();
+
+int main()
+{
+    test_lexicographical_compare();
+    test_pair();
+    test_equal();
+    return 0;
+}
