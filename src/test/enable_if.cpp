5828000242484426ba3026c3b3d81d187afc9e0f
Yonaim <yonaimyo@gmail.com>
2025-10-31 04:40:57 +0900

    Refactor: Apply NAMESPACE macro

--- src/test/enable_if.cpp
+++ src/test/enable_if.cpp
@@ -1,57 +1,53 @@
-
 /*
 Substitution Failure Is Not An Error (SFINAE)
-- 함수 오버로드시, 대입 (substitution)이 실패하더라도 에러로 취급하지 않고 다른 후보를 시도
+- 함수 오버로드 시, 대입(substitution)이 실패하더라도 에러로 취급하지 않고 다른 후보를 시도
 - This rule applies during overload resolution of function templates
 - This feature is used in template metaprogramming.
 */
-// C++98버전에서는 없었다. Boost팀이 만들었다가 역수입됨.
+// C++98 버전에서는 없었다. Boost 팀이 처음 구현했다가 이후 표준에 포함됨.
 
 #include <iostream>
-// #include <type_traits>
-// type_traits 헤더 자체가 C++11부터
 #include "enable_if.h"
 #include "is_floating_point.h"
 #include "is_integral.h"
-
-using namespace ft;
+#include "test_config.h"
 
 // =========================== use in Return Type ==============================
 
 template <typename T>
-typename enable_if<is_integral<T>::value, void>::type printType(T val)
+typename NAMESPACE::enable_if<NAMESPACE::is_integral<T>::value, void>::type printType(T val)
 {
     std::cout << val << " is integral (Return Type)\n";
 }
 
 template <typename T>
-typename enable_if<is_floating_point<T>::value, void>::type printType(T val)
+typename NAMESPACE::enable_if<NAMESPACE::is_floating_point<T>::value, void>::type printType(T val)
 {
     std::cout << val << " is floating-point (Return Type)\n";
 }
 
-// 여러 후보가 동시에 true면 안됨!
-/*
-template <typename T>
-typename enable_if<true, void>::type printType(T val)
-{
-    std::cout << val << " Always true\n"; // Is This Possible?
-}
-*/
+// 여러 후보가 동시에 true면 안 됨!
+// template <typename T>
+// typename NAMESPACE::enable_if<true, void>::type printType(T val)
+// {
+//     std::cout << val << " Always true\n";
+// }
 
 // ========================= use in Parameter Type =============================
 
-// void 포인터인 건, 단순히 기본값(0)을 주기 위한 트릭
+// void 포인터는 기본값(0)을 주기 위한 트릭
 template <typename T>
-void printTypeParam(T val, typename enable_if<is_integral<T>::value, void>::type * = 0)
+void printTypeParam(
+    T val, typename NAMESPACE::enable_if<NAMESPACE::is_integral<T>::value, void>::type * = 0)
 {
     std::cout << val << " is integral (Parameter Type)\n";
 }
 
 // ====================== use in Template Argument Type ========================
 
-// C++98에서 Default template argument는 불가하다. (여기에서 Enable)
-// template <typename T, typename Enable = typename enable_if<is_integral<T>::value, void>::type>
+// C++98에서는 Default template argument가 제한되어 있었음
+// template <typename T, typename Enable = typename
+// NAMESPACE::enable_if<NAMESPACE::is_integral<T>::value, void>::type>
 // void printTypeTemplate(T val)
 // {
 //     std::cout << val << " is integral (Template Argument Type)\n";
