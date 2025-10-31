6cdffb3ac25ccbf52052d7cf3f716ced71df56de
Yonaim <yonaimyo@gmail.com>
2025-10-31 17:30:59 +0900

    Feat: divide test function prototypes

---
+++ include/utils/test.h
@@ -0,0 +1,12 @@
+#ifndef TEST_H
+#define TEST_H
+
+void test_lexicographical_compare();
+void test_pair();
+void test_equal();
+void test_enable_if();
+void test_is_integral();
+void test_iterator_traits();
+void test_reverse_iterator();
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,3 @@
-void test_lexicographical_compare();
-void test_pair();
-void test_equal();
-void test_enable_if();
-void test_is_integral();
-void test_iterator_traits();
-void test_reverse_iterator();
 
 int main()
 {
