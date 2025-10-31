f0a5fd11e0b539c2234cbb789b61bafe690135b0
Yonaim <yonaimyo@gmail.com>
2025-10-31 16:58:08 +0900

    Feat: update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 void test_lexicographical_compare();
 void test_pair();
 void test_equal();
+void test_enable_if();
+void test_is_integral();
+void test_iterator_traits();
+void test_reverse_iterator();
 
 int main()
 {
-    test_lexicographical_compare();
-    test_pair();
-    test_equal();
+    // test_lexicographical_compare();
+    // test_pair();
+    // test_equal();
+    // test_enable_if();
+    // test_is_integral();
+    // test_iterator_traits();
+    // test_reverse_iterator();
     return 0;
 }
