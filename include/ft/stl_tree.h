6ab13adb24a12afa922802518edff3362351f919
Yonaim <yonaimyo@gmail.com>
2025-11-21 21:03:31 +0900

    Feat: Add prototypes of tree API functions

--- include/ft/stl_tree.h
+++ include/ft/stl_tree.h
@@ -135,7 +135,7 @@ namespace ft
         _node_ptr _node;
     };
 
-    // ==================== Rb_tree ====================
+    // =============================== Rb_tree =================================
 
     // Helper type to manage default initialization of node count and header.
     struct _Rb_tree_header
@@ -149,11 +149,92 @@ namespace ft
     // KeyOfValue 덕분에 associative array까지 커버하는 '범용 트리 엔진'
     template <typename _Key, typename _Val, typename _KeyOfValue, typename _Compare,
               typename _Alloc = allocator<_Val>>
-    struct _Rb_tree
+    class _Rb_tree
     {
+      public:
+        // key & value
+        typedef _Key              key_type;
+        typedef _Val              value_type;
+        typedef value_type       *pointer;
+        typedef const value_type *const_pointer;
+        typedef value_type       &reference;
+        typedef const value_type &const_reference;
+
+        // allocator
+        typedef _Alloc                  allocator_type;
+        typedef _Alloc::difference_type difference_type;
+        typedef _Alloc::size_type       size_type;
+
+        // node
+        typedef _Rb_tree_node<_Val> _Node;
+        typedef _Rb_tree_node_base  _Base;
+        typedef _Node              *_Node_ptr;
+        typedef _Base              *_Base_ptr;
+
+        // iterator
         typedef _Rb_tree_iterator       iterator;
         typedef _Rb_tree_const_iterator const_iterator;
-        // TODO
+
+        // ==================== API ====================
+        // constructors
+        _Rb_tree();
+        _Rb_tree(const _Compare &, const allocator_type &);
+        ~_Rb_tree();
+
+        // iterators
+        iterator       begin();
+        const_iterator begin() const;
+        iterator       end();
+        const_iterator end() const;
+        bool           empty() const;
+        size_type      size() const;
+
+        // find
+        iterator       find(const key_type &k);
+        const_iterator find(const key_type &k) const;
+        iterator       find(const key_type &k);
+        const_iterator find(const key_type &k) const;
+
+        // lower_bound / upper_bound / equal_range
+        iterator                             lower_bound(const key_type &k);
+        const_iterator                       lower_bound(const key_type &k) const;
+        iterator                             upper_bound(const key_type &k);
+        const_iterator                       upper_bound(const key_type &k) const;
+        pair<iterator, iterator>             equal_range(const key_type &k);
+        pair<const_iterator, const_iterator> equal_range(const key_type &k) const;
+
+        // insert
+        // map & set에서는 unique, multimap & multiset에서는 equal 사용
+        pair<iterator, bool> insert_unique(const value_type &v);
+        iterator             insert_equal(const value_type &v);
+        iterator             insert_unique(iterator position, const value_type &v);
+        iterator             insert_equal(iterator position, const value_type &v);
+
+        // erase
+        /*
+            왜 하필 이름이 erase?
+            - remove는 std::remove에서 사용됨.
+                std::remove는 '앞으로 옮겨서' 지워진 것처럼 보이게하는 함수이고
+                할당된 공간을 회수하지 않음
+            - destroy는 'destructor만 호출'을 의미 (allocator에서 사용)
+            - delete는 예약된 C++ 키워드
+        */
+        void      erase(iterator position);
+        size_type erase(const key_type &k);
+        void      erase(iterator first, iterator last);
+
+      private:
+        // node create & destroy
+        _Node_ptr _create_node(const value_type &v);
+        void      _destroy_node(_Node_ptr);
+
+        // balancing (fixup)
+        void _insert_fixup(_Base_ptr x);
+        void _erase_fixup(_Base_ptr x, _Base_ptr x_parent);
+
+        // find min / max node (stateless)
+        static _Node_ptr _minimum(_Node_ptr x);
+        static _Node_ptr _maximum(_Node_ptr x);
     };
 
 } // namespace ft
