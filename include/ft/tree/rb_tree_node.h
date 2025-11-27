d642f4ecaa3fce701884317b8bfcd4d537979c6f
yonaMac <yonaimyo@gmail.com>
2025-11-27 13:32:24 +0900

    Comment: rb_tree_node

--- include/ft/tree/rb_tree_node.h
+++ include/ft/tree/rb_tree_node.h
@@ -14,7 +14,7 @@ namespace ft
     };
 
     // value 미포함
-    // 일반 노드 클래스는 이 base를 상속받음
+    // 일반 노드 클래스(_Rb_tree_node)는 이 base를 상속받음
     // header 노드의 경우 value 미포함이므로 이 base를 상속받는다
     struct _Rb_tree_node_base
     {
@@ -25,6 +25,14 @@ namespace ft
         _Base_ptr                   left;
         _Base_ptr                   right;
 
+        /*
+            최소/최대 노드를 찾는 연산:
+            - value_type가 관계가 없다
+            - 트리 구조와 상관 없는 포인터 기반의 연산
+
+            base node 구조체에 연산을 정의하는 이유???
+            -> value를 가지지 않는 노드(header)도 존재하므로, base 레벨에서 연산을 정의해야한다.
+        */
         static _Base_ptr _minumum(_Base_ptr ptr)
         {
             while (ptr->left != 0)
