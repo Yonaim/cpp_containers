20d74e487d50b81adf3c35108b23fcfdaa8a4a76
Yonaim <yonaimyo@gmail.com>
2025-12-26 09:03:53 +0900

    Feat: Add is_class

---
+++ include/ft/type_traits/is_class.h
@@ -0,0 +1,31 @@
+#ifndef FT_IS_CLASS_H
+#define FT_IS_CLASS_H
+
+// class 혹은 struct일시 value = true
+namespace ft
+{
+    // 바이트 수로 yes/no 구분 (바이트 수에 큰 의미는 없음, 단지 구분용)
+    typedef char yes_type; // 1바이트
+    struct no_type         // 2바이트
+    {
+        char dummy[2];
+    };
+
+    template <class T>
+    class is_class
+    {
+      private:
+        yes_type check(int T::*); // T 내부 멤버 중 int를 가리키는 포인터
+        no_type  check(...);
+
+      public:
+        // sizeof()의 피연산자는 evaluation되지 않음
+        // 따라서 check()가 호출될 일은 없다 -> 선언만 하고 구현 안 해도됨
+        enum
+        {
+            value = sizeof(check(0)) == sizeof(yes_type)
+        };
+    };
+} // namespace ft
+
+#endif
